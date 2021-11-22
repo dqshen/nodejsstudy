74c68679a1f6fcb0fbe1d1e92340a17e6f35f656
dqs <dqs@dqs.com>
2021-11-22 09:38:33 +0800

    add header directive to hello_node.js

--- hello_node.js
+++ hello_node.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.writeHead(200,{
+        'Content-Type':'text/plain',
+        'X-Foo':'bar'
+    });
     res.end('Hello Node.js\n');
 }).listen(3000,"127.0.0.1");
 
