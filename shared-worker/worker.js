8557d799267b2a957989506818a89b77dc17776b
dqs <dqs@dqs.com>
2021-12-14 17:04:48 +0800

    add static variable for sharedworker

--- shared-worker/worker.js
+++ shared-worker/worker.js
@@ -1,8 +1,10 @@
+var count = 0;
+
 onconnect = function(e) {
   var port = e.ports[0];
 
   port.onmessage = function(e) {
-    var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
+    var workerResult = `Result${count++}: ` + (e.data[0] * e.data[1]);
     port.postMessage(workerResult);
   }
 
